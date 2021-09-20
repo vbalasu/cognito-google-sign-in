3ac140b405eb500499a6212294de24cebf835d87
vbalasu <vbalasu@gmail.com>
2021-09-20 16:41:57 -0700

    Updated to AWS SDK v3 for smaller bundle

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-let AWS = require('aws-sdk')
-AWS.config.region = 'us-east-1';
-AWS.config.credentials = new AWS.CognitoIdentityCredentials({IdentityPoolId: 'us-east-1:bca7d5a3-1886-4529-975a-eba315dd5793'});
-dynamodb = new AWS.DynamoDB();
-function handleListTables(err, data) {
-  if (err) console.log(err, err.stack);
-  else console.log(data);
+async function main() {
+  let { DynamoDBClient, ListTablesCommand } = require('@aws-sdk/client-dynamodb');
+  const {CognitoIdentityClient} = require("@aws-sdk/client-cognito-identity");
+  const {fromCognitoIdentityPool} = require("@aws-sdk/credential-provider-cognito-identity");
+  const REGION = 'us-east-1';
+  const credentials = fromCognitoIdentityPool({
+    client: new CognitoIdentityClient({region:REGION}),
+    identityPoolId: 'us-east-1:bca7d5a3-1886-4529-975a-eba315dd5793'});
+  const dynamodb = new DynamoDBClient({region: REGION, credentials: credentials});
+  let params = {};
+  const command = new ListTablesCommand(params);
+  let data = await dynamodb.send(command);
+  console.log(data.TableNames);
 }
-dynamodb.listTables({}, handleListTables);
+main();
