dee6f7bfb64ff5893fca1f8b4514c2a0ecd50bb4
vbalasu <vbalasu@gmail.com>
2021-09-20 16:04:19 -0700

    Working in the browser

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ let AWS = require('aws-sdk')
 AWS.config.region = 'us-east-1';
 AWS.config.credentials = new AWS.CognitoIdentityCredentials({IdentityPoolId: 'us-east-1:bca7d5a3-1886-4529-975a-eba315dd5793'});
 dynamodb = new AWS.DynamoDB();
+function handleListTables(err, data) {
+  if (err) console.log(err, err.stack);
+  else console.log(data);
+}
+dynamodb.listTables({}, handleListTables);
