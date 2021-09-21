b540fd19249c192632f82d585e6dd606922213aa
vbalasu <vbalasu@gmail.com>
2021-09-21 15:12:25 -0700

    Documented cors and iam policy, implemented logic to write current date if member-since.txt is not found

--- src/index.js
+++ src/index.js
@@ -22,14 +22,36 @@ async function afterSignIn() {
   var credentials = new fromCognitoIdentityPool({
     client: new CognitoIdentityClient({region:REGION}),
     identityPoolId: 'us-east-1:bca7d5a3-1886-4529-975a-eba315dd5793', logins: {'accounts.google.com': window.id_token}});
-  const { S3Client, GetObjectCommand } = require("@aws-sdk/client-s3"); // CommonJS import
+  window.credentials = await credentials();
+
+  const { S3Client, GetObjectCommand, HeadObjectCommand, PutObjectCommand } = require("@aws-sdk/client-s3"); // CommonJS import
   s3 = new S3Client({region: REGION, credentials: credentials});
-  var input = {Bucket: 'profile.cloudmatica.com', Key: 'deletethis.txt'}
-  const command = new GetObjectCommand(input);
-  response = await s3.send(command);
-  data = await response.Body.getReader().read();
-  stringdata = new TextDecoder().decode(data.value);
-  console.log(stringdata);
+  var bucket_name = 'profile.cloudmatica.com';
+  var key = 'cognito/profile/' + window.credentials.identityId + '/member-since.txt';
+  var input = {Bucket: bucket_name, Key: key};
+  const headCommand = new HeadObjectCommand(input);
+  try {
+    response = await s3.send(headCommand);
+    const command = new GetObjectCommand(input);
+    response = await s3.send(command);
+    data = await response.Body.getReader().read();
+    stringdata = new TextDecoder().decode(data.value);
+    console.log(stringdata);
+  }
+  catch (err) {
+    console.log('key does not exist');
+    if(err.$metadata.httpStatusCode == 403) {
+      input.Body = new Date().toISOString();
+      const putCommand = new PutObjectCommand(input);
+      try {
+        response = await s3.send(putCommand);
+        console.log(input);
+      }
+      catch (err) {
+        console.log(err);
+      }
+    }
+  }
 }
 
 async function main() {
