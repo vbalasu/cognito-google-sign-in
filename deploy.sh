8b10d4fcc61ccf5d9280fff329db0bff0504e219
vbalasu <vbalasu@gmail.com>
2021-09-21 10:12:58 -0700

    Added deployment script

---
+++ deploy.sh
@@ -0,0 +1,2 @@
+aws s3 cp --acl public-read dist/index.html s3://profile.cloudmatica.com/index.html
+aws s3 cp --acl public-read dist/bundle.js s3://profile.cloudmatica.com/bundle.js
---
+++ dist/_e3ad.bundle.js
@@ -0,0 +1,21 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkcognito_google_sign_in"] = self["webpackChunkcognito_google_sign_in"] || []).push([["_e3ad"],{
+
+/***/ "?e3ad":
+/*!*******************************************!*\
+  !*** @aws-sdk/signature-v4-crt (ignored) ***!
+  \*******************************************/
+/***/ (() => {
+
+eval("/* (ignored) */\n\n//# sourceURL=webpack://cognito-google-sign-in/@aws-sdk/signature-v4-crt_(ignored)?");
+
+/***/ })
+
+}]);
